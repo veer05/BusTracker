e292f65882e9bcc589d0cb8437858e4a0a8c84e6
veer05 <veeresh05@gmail.com>
2018-04-13 23:30:01 -0400

    initial_setup

---
+++ bustracker/lib/bustracker_web/controllers/fallback_controller.ex
@@ -0,0 +1,20 @@
+defmodule BustrackerWeb.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use BustrackerWeb, :controller
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> render(BustrackerWeb.ChangesetView, "error.json", changeset: changeset)
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> render(BustrackerWeb.ErrorView, :"404")
+  end
+end
